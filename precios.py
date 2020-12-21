0a247acab7524bb0049d6a4cccfef667b563daa5
GabsRTX <>
2020-12-21 14:12:53 -0600

    inicio

---
+++ precios.py
@@ -0,0 +1,115 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+import precios_ui
+
+import requests
+
+import time
+
+class Worker(QThread):
+
+	cripto_values = pyqtSignal(dict)
+
+	def run(self):
+
+		while(True):
+
+			response = requests.get('https://api.bitso.com/v3/ticker/')
+
+			json_response = response.json()
+
+			# 0 btc_mxn
+			# 1 eth_btc
+			# 2 eth_mxn
+			# 3 xrp_btc
+			# 4 xrp_mxn
+			# 5 ltc_btc
+			# 6 ltc_mxn
+			# 7bch_btc
+			# 8 bch_mxn
+			# 9 tusd_btc
+			# 10 tusd_mxn
+			# 11 mana_btc
+			# 12 mana_mxn
+			# 13 gnt_btc
+			# 14 gnt_mxn
+			# 15 bat_btc
+			# 16 bat_mxn
+			# 17 btc_ars
+			# 18 btc_dai
+			# 19 dai_mxn
+			# 20 btc_usd
+			# 21 xrp_usd
+			# 22 eth_usd
+			# 23 dai_ars
+
+			self.cripto_values.emit({
+				"bitcoin": {
+					'book': json_response['payload'][0]['book'],
+					'last': json_response['payload'][0]['last']
+				},
+				"xrp": {
+					'book': json_response['payload'][4]['book'],
+					'last': json_response['payload'][4]['last']
+				},
+				'litecoin': {
+					'book': json_response['payload'][6]['book'],
+					'last': json_response['payload'][6]['last']
+				},
+				'ether': {
+					'book': json_response['payload'][2]['book'],
+					'last': json_response['payload'][2]['last']
+				}
+			})
+
+			time.sleep(1)
+
+class MainWindow(QtWidgets.QMainWindow, object):
+
+	def __init__(self):
+
+		try:
+			
+			super(MainWindow, self).__init__()
+
+			self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
+			self.ui = precios_ui.Ui_MainWindow()
+
+			self.ui.setupUi(self)
+
+			self.thread = QThread()
+
+			self.worker = Worker()
+
+			self.worker.moveToThread(self.thread)
+
+			self.thread.started.connect(self.worker.run)
+			self.worker.cripto_values.connect(self.print_value)
+
+			self.thread.start()
+
+		except Exception as e:
+			
+			print(str(e))
+
+	def print_value(self, monedas):
+
+		self.ui.qt_tabla_precios.setRowCount(0)
+
+		for fila_numero, key in enumerate(monedas):
+
+			self.ui.qt_tabla_precios.insertRow(fila_numero)
+
+			self.ui.qt_tabla_precios.setItem(fila_numero, 0, QtWidgets.QTableWidgetItem(str(monedas[key]['book'])))
+			self.ui.qt_tabla_precios.setItem(fila_numero, 1, QtWidgets.QTableWidgetItem(str(monedas[key]['last'])))
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication([])
+	application = MainWindow()
+	application.show()
+	app.exec_()
---
+++ precios_ui.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'precios_ui.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(270, 170)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.qt_tabla_precios = QtWidgets.QTableWidget(self.centralwidget)
+        self.qt_tabla_precios.setGeometry(QtCore.QRect(10, 10, 250, 150))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(12)
+        self.qt_tabla_precios.setFont(font)
+        self.qt_tabla_precios.setObjectName("qt_tabla_precios")
+        self.qt_tabla_precios.setColumnCount(2)
+        self.qt_tabla_precios.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.qt_tabla_precios.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        item.setFont(font)
+        self.qt_tabla_precios.setHorizontalHeaderItem(1, item)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        item = self.qt_tabla_precios.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "MONEDA"))
+        item = self.qt_tabla_precios.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "PRECIO"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
